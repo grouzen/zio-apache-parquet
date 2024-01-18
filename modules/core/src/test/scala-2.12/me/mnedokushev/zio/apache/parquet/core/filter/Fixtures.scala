b928a51184f5792bc9fadff4bac1e84c42e75ad9
Michael Nedokushev <michael.nedokushev.-nd@disneystreaming.com>
2024-01-18 01:14:58 +0000

    Add simple test for all primitive types

--- modules/core/src/test/scala-2.12/me/mnedokushev/zio/apache/parquet/core/filter/Fixtures.scala
+++ modules/core/src/test/scala-2.12/me/mnedokushev/zio/apache/parquet/core/filter/Fixtures.scala
@@ -4,35 +4,91 @@ import me.mnedokushev.zio.apache.parquet.core.Value
 import org.apache.parquet.filter2.predicate.FilterApi
 import org.apache.parquet.filter2.predicate.Operators.BinaryColumn
 import org.apache.parquet.io.api.Binary
+import zio._
 import zio.schema._
+
+import java.time._
+import java.util.UUID
 
 object Fixtures {
 
   case class MyRecord(a: String, b: Int, child: MyRecord.Child)
 
   object MyRecord {
-    implicit val schema                     = DeriveSchema.gen[MyRecord]
-    implicit val typeTag: TypeTag[MyRecord] = Derive.derive[TypeTag, MyRecord](TypeTagDeriver.default)
+    implicit val schema                     =
+      DeriveSchema.gen[MyRecord]
+    implicit val typeTag: TypeTag[MyRecord] =
+      Derive.derive[TypeTag, MyRecord](TypeTagDeriver.default)
 
     case class Child(c: Int, d: Option[Long])
     object Child {
-      implicit val schema                  = DeriveSchema.gen[Child]
-      implicit val typeTag: TypeTag[Child] = Derive.derive[TypeTag, Child](TypeTagDeriver.default)
+      implicit val schema                  =
+        DeriveSchema.gen[Child]
+      implicit val typeTag: TypeTag[Child] =
+        Derive.derive[TypeTag, Child](TypeTagDeriver.default)
     }
 
   }
 
   case class MyRecordSummoned(a: Int, b: String)
 
-  object MyRecordSummoned {
-    implicit val schema = DeriveSchema.gen[MyRecordSummoned]
+  object MyRecordSummoned  {
+    implicit val schema =
+      DeriveSchema.gen[MyRecordSummoned]
 
     implicit val intTypeTag: TypeTag.EqNotEq[Int]   =
       TypeTag.eqnoteq[Int, Binary, BinaryColumn](
         FilterApi.binaryColumn,
         v => Value.string(v.toString).value
       )
-    implicit val typeTag: TypeTag[MyRecordSummoned] = Derive.derive[TypeTag, MyRecordSummoned](TypeTagDeriver.summoned)
+    implicit val typeTag: TypeTag[MyRecordSummoned] =
+      Derive.derive[TypeTag, MyRecordSummoned](TypeTagDeriver.summoned)
+  }
+
+  case class MyRecordAllTypes1(
+    string: String,
+    boolean: Boolean,
+    byte: Byte,
+    short: Short,
+    int: Int,
+    long: Long,
+    float: Float,
+    double: Double,
+    binary: Chunk[Byte],
+    char: Char,
+    uuid: UUID,
+    bigDecimal: java.math.BigDecimal,
+    bigInteger: java.math.BigInteger,
+    dayOfWeek: DayOfWeek,
+    month: Month,
+    monthDay: MonthDay,
+    period: Period,
+    year: Year,
+    yearMonth: YearMonth,
+    zoneId: ZoneId,
+    zoneOffset: ZoneOffset
+  )
+  object MyRecordAllTypes1 {
+    implicit val schema                              =
+      DeriveSchema.gen[MyRecordAllTypes1]
+    implicit val typeTag: TypeTag[MyRecordAllTypes1] =
+      Derive.derive[TypeTag, MyRecordAllTypes1](TypeTagDeriver.default)
+  }
+  case class MyRecordAllTypes2(
+    duration: Duration,
+    instant: Instant,
+    localDate: LocalDate,
+    localTime: LocalTime,
+    localDateTime: LocalDateTime,
+    offsetTime: OffsetTime,
+    offsetDateTime: OffsetDateTime,
+    zonedDateTime: ZonedDateTime
+  )
+  object MyRecordAllTypes2 {
+    implicit val schema                              =
+      DeriveSchema.gen[MyRecordAllTypes2]
+    implicit val typeTag: TypeTag[MyRecordAllTypes2] =
+      Derive.derive[TypeTag, MyRecordAllTypes2](TypeTagDeriver.default)
   }
 
 }
