b79d964712e9f1045d48dc822bd15490714c8146
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-18 00:21:03 +0100

    Update scala3-library to 3.3.3 (#27)
    * Update scala3-library to 3.3.3
    
    * Regenerate GitHub Actions workflow
    
    Executed command: sbt githubWorkflowGenerate

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -15,7 +15,7 @@ object BuildHelper {
 
   val Scala212 = "2.12.18"
   val Scala213 = "2.13.12"
-  val Scala3   = "3.3.1"
+  val Scala3   = "3.3.3"
 
   private def betterMonadicFor(scalaVersion: String) =
     CrossVersion.partialVersion(scalaVersion) match {
