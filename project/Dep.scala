34f1430735e17a5393dde459870fe8e1a8f397d4
Scala Steward <scala_steward@virtuslab.com>
2024-04-17 15:27:11 +0000

    Update scala-collection-compat to 2.12.0

--- project/Dep.scala
+++ project/Dep.scala
@@ -5,7 +5,7 @@ object Dep {
   object V {
     val zio                   = "2.0.21"
     val zioSchema             = "1.1.0"
-    val scalaCollectionCompat = "2.11.0"
+    val scalaCollectionCompat = "2.12.0"
     val apacheParquet         = "1.13.1"
     val apacheHadoop          = "3.4.0"
   }
