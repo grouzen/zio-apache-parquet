a47653813d4def1a0d23ed294fd4f9112230271f
Scala Steward <scala_steward@virtuslab.com>
2024-09-06 15:02:44 +0000

    Update scala-library to 2.12.20

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -23,7 +23,7 @@ object BuildHelper {
     scalacOptions ++= source3Compatibility(scalaVersion.value)
   )
 
-  val Scala212 = "2.12.19"
+  val Scala212 = "2.12.20"
   val Scala213 = "2.13.14"
   val Scala3   = "3.5.0"
 
