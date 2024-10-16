e85e3b445da0e81ba9764542fe8671e7b80f8e91
Scala Steward <scala_steward@virtuslab.com>
2024-10-16 16:32:58 +0000

    Update zio-apache-parquet-core to 0.1.3

--- docs/scala-cli/Value.scala
+++ docs/scala-cli/Value.scala
@@ -1,5 +1,5 @@
 //> using scala "3.5.0"
-//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.1
+//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.3
 
 import zio.schema.*
 import me.mnedokushev.zio.apache.parquet.core.codec.*
