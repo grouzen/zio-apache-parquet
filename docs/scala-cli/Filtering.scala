3c5f459f615b090988c4eb207b199a7f94457e32
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-12-27 18:30:37 +0100

    Update zio-apache-parquet-core to 0.1.6 (#107)

--- docs/scala-cli/Filtering.scala
+++ docs/scala-cli/Filtering.scala
@@ -1,5 +1,5 @@
 //> using scala "3.5.0"
-//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.5
+//> using dep me.mnedokushev::zio-apache-parquet-core:0.1.6
 
 import zio.*
 import zio.schema.*
