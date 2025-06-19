2dbfbe255bfb59568b6cfbbf32e4648468bb61e8
Scala Steward <scala_steward@virtuslab.com>
2025-06-19 16:42:44 +0000

    Update zio-schema, zio-schema-derivation to 1.7.3

--- project/Dep.scala
+++ project/Dep.scala
@@ -5,7 +5,7 @@ object Dep {
 
   object V {
     val zio                   = "2.1.19"
-    val zioSchema             = "1.7.2"
+    val zioSchema             = "1.7.3"
     val scalaCollectionCompat = "2.13.0"
     val apacheParquet         = "1.15.2"
     val apacheHadoop          = "3.4.1"
