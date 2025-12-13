41740c7fac52cf765c5ea3c87cf5b2582af27d24
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-12-13 17:29:35 +0100

    Update scala-library, scala-reflect to 2.13.18 (#211)
    Co-authored-by: Michael Nedokushev <grouzen.hexy@gmail.com>

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -23,7 +23,7 @@ object BuildHelper {
   )
 
   val Scala212 = "2.12.21"
-  val Scala213 = "2.13.17"
+  val Scala213 = "2.13.18"
   val Scala3   = "3.3.6"
 
   private def betterMonadicFor(scalaVersion: String) =
