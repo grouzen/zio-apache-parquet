b8a9ff815fb1b9c42e424e4679692eab63593899
Scala Steward <scala_steward@virtuslab.com>
2026-06-12 17:20:44 +0000

    Update scala3-library to 3.3.8

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -24,7 +24,7 @@ object BuildHelper {
 
   val Scala212 = "2.12.21"
   val Scala213 = "2.13.18"
-  val Scala3   = "3.3.6"
+  val Scala3   = "3.3.8"
 
   private def betterMonadicFor(scalaVersion: String) =
     CrossVersion.partialVersion(scalaVersion) match {
